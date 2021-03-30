bca7bd651bfa56b220e6701aa1541b53a0388aff
P-Jeremy <jemyplu@gmail.com>
2021-03-30 11:31:09 +0200

    Better use of _playfor method

--- statement.js
+++ statement.js
@@ -12,17 +12,15 @@ function statement(invoice, plays) {
     minimumFractionDigits: 2,
   }).format;
   for (let perf of invoice.performances) {
-    const play = _playFor(perf);
-
-    const thisAmount = _amountFor(perf, play);
+    const thisAmount = _amountFor(perf, _playFor(perf));
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === _playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+    result += `  ${_playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += thisAmount;
